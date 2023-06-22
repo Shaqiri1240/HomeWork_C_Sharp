fe4c8755662a2f9855ea0ef8e58bd3cd2ed1a4c6
Vladislav <shoker1240@gmail.com>
2023-06-22 15:34:26 +0300

    First seminar

---
+++ task04/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное 
+число и на выходе показывает последнюю цифру этого числа.*/
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100 || number > 999) Console.WriteLine("Необходимо трёхзначное число!");
+else {
+int count = number % 10;
+Console.Write(count);
+}
