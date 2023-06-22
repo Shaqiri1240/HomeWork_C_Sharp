b1944464ea003fd8685a28f793591cf6ea5af0ec
Vladislav <shoker1240@gmail.com>
2023-06-22 16:55:34 +0300

    Complete task07

---
+++ hw_task07/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Напишите программу, которая на вход п
+ринимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет */
+
+Console.WriteLine("Введите число");
+int numA = Convert.ToInt32(Console.ReadLine());
+if (numA % 2 == 0) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
