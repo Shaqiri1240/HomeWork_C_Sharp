f5ca72e3a900dfb60b4ccf8cf09160cf360e4441
Vladislav <shoker1240@gmail.com>
2023-06-29 14:54:25 +0300

    complete third seminar

---
+++ task17/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Напишите программу, которая принимает на вход координаты точки (X и Y), 
+причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+*/
+int GetNumberOfQuadrant (int X, int Y)
+{
+    int result = 0;
+    if (X > 0 && Y > 0)
+    {
+        result = 1;
+    }
+    else if (X < 0 && Y > 0)
+    {
+        result = 2;
+    }
+    else if (X < 0 && Y < 0)
+    {
+        result = 3;
+    }
+    else if (X > 0 && Y < 0)
+    {
+        result = 4;
+    }
+    return result;
+}
+
+Console.WriteLine("Введи переменную X");
+int userX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи переменную Y");
+int userY = Convert.ToInt32(Console.ReadLine());
+int quadrant = GetNumberOfQuadrant(userX, userY);
+if (quadrant > 0)
+{
+    Console.WriteLine($"Точка [{userX}: {userY}] находится в {quadrant} плоскости");
+}
+else
+{
+    Console.WriteLine($"Точка [{userX}: {userY}] находится на пересечении плоскости");
+}
---
+++ task18/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Напишите программу, которая по заданному номеру четверти,
+показывает диапазон возможных координат точек в этой четверти */
+
+string QuadrantRange(int quadrant)
+{    
+    if (quadrant == 1) return "x > 0; y > 0";
+  else if (quadrant == 2) return "x < 0; y > 0";
+   else if (quadrant == 3) return "x < 0; y < 0";
+  else if (quadrant == 4) return "x > 0; y < 0";
+  else return "invalid quadrant";
+}
+Console.Write("Enter a quadrant number: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(QuadrantRange(quarter));
