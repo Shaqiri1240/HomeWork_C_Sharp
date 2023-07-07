7728acb65129031f95174546542d3678a2b4b0ab
Vladislav <shoker1240@gmail.com>
2023-07-08 00:49:03 +0300

    complete four homework

---
+++ hw_task25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+int GetNatureDegree(int number,int degree)
+{
+    int result = number;
+    for (int i = 1; i <= degree-1; i++)
+    {
+        result *= number;
+    }
+    return result;
+}
+Console.WriteLine("Введите первое число");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numB = Convert.ToInt32(Console.ReadLine());
+int nature = GetNatureDegree(numA,numB);
+Console.WriteLine(nature);
---
+++ hw_task27/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int FindLength(int number)
+{
+    int count = 0;
+     for (int i = 0;number > 0;i++)
+    {
+        number /= 10;
+        count++;
+    }
+    return count;
+}
+int Sum(int number, int length)
+{
+    int sum = 0;
+    for (int i = 1; i <= length; i++)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+Console.WriteLine("Введите ваше число.");
+int number = Convert.ToInt32(Console.ReadLine());
+int length = FindLength(number);
+Console.Write($"Сумма цифр в числе {number} равна: {Sum(number,length)}");
